498b34680e63556db7efa006e20faaa234440a15
Andrew Voss <voss1167@gmail.com>
2018-01-07 15:34:10 -0600

    fixed argv

--- Robot.py
+++ Robot.py
@@ -2,11 +2,11 @@
 # Reason for this file: Less latency
 
 # Example:
-import argv
+from sys import argv
 import robotonomy.RoboPiLib as RPL
 import setup
 
-leftMotor, rightMotor = argv
+pyProgam, leftMotor, rightMotor = argv
 
 RPL.servoWrite(0,leftMotor)
 RPL.servoWrite(1,rightMotor)
